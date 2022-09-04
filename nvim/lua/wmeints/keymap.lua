5fbae452d8f5473472b5d6083e7ce48080d4c656
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-04 11:52:59 +0200

    Add formatting

--- nvim/lua/wmeints/keymap.lua
+++ nvim/lua/wmeints/keymap.lua
@@ -2,3 +2,6 @@ local nnoremap = require("wmeints.remap").nnoremap
 local vnoremap = require("wmeints.remap").vnoremap
 
 nnoremap("<leader>t", ":NERDTreeToggle<CR>")
+
+nnoremap("<leader>f", "<Plug>(coc-format-selected)")
+vnoremap("<leader>f", "<Plug>(coc-format-selected)")
