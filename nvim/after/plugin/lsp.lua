c9df6c5b96ad53a9f1c160e8a8174041a95c634e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-07 21:13:27 +0200

    Add Rust analyzer LSP config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,1 +1,22 @@
 require("nvim-lsp-installer").setup {}
+
+local lsp_flags = {
+    debounce_txt_changes = 150
+}
+
+require("lspconfig").pyright.setup{
+    lsp_flags =  lsp_flags
+}
+
+require("lspconfig").tsserver.setup{
+    lsp_flags = lsp_flags
+}
+
+require('lspconfig').rust_analyzer.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
