e4986d0576fc98dc57f8fc7d36f7c36f06c39094
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-10-08 16:14:00 +0200

    feat: Add Ctrl+C to cancel edit mode

--- nvim/after/plugin/keymap.lua
+++ nvim/after/plugin/keymap.lua
@@ -1,5 +1,6 @@
 local nnoremap = require("wmeints.remap").nnoremap
 local vnoremap = require("wmeints.remap").vnoremap
+local inoremap = require("wmeints.remap").inoremap
 
 -- Plugin actions
 nnoremap("<leader>t", ":NERDTreeToggle<CR>")
@@ -11,3 +12,6 @@ nnoremap("<c-h>","<c-w>h")
 nnoremap("<c-j>","<c-w>j")
 nnoremap("<c-k>","<c-w>k")
 nnoremap("<c-l>","<c-w>l")
+
+-- Awesome but deadly
+inoremap("<C-c>", "<Esc>")
